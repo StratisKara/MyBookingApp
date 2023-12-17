ce5b6b594a188e1a54e5f5f354b3100ceed7420f
StratisKara <105735724+StratisKara@users.noreply.github.com>
2023-12-17 05:05:27 +0000

    Add files via upload

---
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using BookingApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookingApp.Areas.Identity.Pages.Account.Manage
+{
+    public partial class IndexModel : PageModel
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public IndexModel(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
+        {
+            [Display(Name = "Prénom")]
+            public string FirstName { get; set; }
+
+            [Display(Name = "Nom")]
+            public string LastName { get; set; }
+
+            [Phone]
+            [Display(Name = "N° de téléphone")]
+            public string PhoneNumber { get; set; }
+
+        }
+
+        private void Load(User user)
+        {
+            var firstName = user.FirstName;
+            var lastName = user.LastName;
+            var phoneNumber = user.PhoneNumber;
+
+            Input = new InputModel
+            {
+                LastName = lastName,
+                FirstName = firstName,
+                PhoneNumber = phoneNumber
+            };
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            Load(user);
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                Load(user);
+                return Page();
+            }
+
+            user.FirstName = Input.FirstName;
+            user.LastName = Input.LastName;
+            user.PhoneNumber = Input.PhoneNumber;
+
+            // Update user in database
+            await _userManager.UpdateAsync(user);
+
+            // Update signin user informations
+            await _signInManager.RefreshSignInAsync(user);
+            StatusMessage = "Votre profil a été mis a jour";
+            return RedirectToPage();
+        }
+    }
+}
